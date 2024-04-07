fc6f043365e2a29be5ea8a9bb7d8e31cf45f9965
Arif-Hajiyev <164586730+Arif-Hajiyev@users.noreply.github.com>
2024-04-07 18:03:00 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,9 +1,14 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+  a=input('Enter a letter of the alphabet: ')
+  b='aeiou'
+  if a in b:
+    print('Entered alphabet is a vowel!')
+  elif a=='y':
+    print('Sometimes it is a vowel, and sometimes it is a consonant!')
+  else:
+    print('Entered alphabet is a consonant!')
   pass
 
 if __name__ == "__main__":
